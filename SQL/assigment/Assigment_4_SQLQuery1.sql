ac469187b3a4f041b1ff216aeda2bc5ea0893702
faruk köse <frkks47@gmail.com>
2022-03-18 21:30:16 +0300

    17.03.2022

--- SQL/assigment/Assigment_4_SQLQuery1.sql
+++ SQL/assigment/Assigment_4_SQLQuery1.sql
@@ -1,18 +1,31 @@
 
 
 
-with t1 as
-(
-
 select	product_id, discount, sum(quantity) quantity1		
 from	sale.order_item
 GROUP BY 
 	GROUPING SETS ((discount, product_id))
 order by product_id
-)
 
-select product_id, discount,
-	lead(quantity1) over (partition by product_id order by dicount) lead_,
-	row_number () over (partition by product_id order by dicount) rows_
-from t1
-order by  product_id, discount
+
+
+
+WITH T1 AS
+(
+SELECT	product_id,
+		discount,
+		LEAD(SUM(quantity)) OVER (PARTITION BY product_id ORDER BY discount) Top_Sales,
+		CASE
+			WHEN ((LEAD(SUM(quantity)) OVER (PARTITION BY product_id ORDER BY discount)) - SUM(quantity)) < 0 THEN 'NEGATIVE'
+			WHEN ((LEAD(SUM(quantity)) OVER (PARTITION BY product_id ORDER BY discount)) - SUM(quantity)) = 0 THEN 'NEUTRAL'
+			WHEN ((LEAD(SUM(quantity)) OVER (PARTITION BY product_id ORDER BY discount)) - SUM(quantity)) > 0 THEN 'POSITIVE'
+			END Dis_Effect
+FROM sale.order_item
+GROUP BY product_id, discount
+
+) 
+
+SELECT product_id, discount,Dis_Effect
+FROM T1
+ORDER BY product_id
+
